fce8f273864417eb910f9674a8494311f9e2b5fc
fherrerao <fherrera0206@gmail.com>
2022-04-01 10:45:16 -0500

    Verify the solution

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,19 +1,19 @@
 module MyEnumerable
   def all?
-    comp = true;
-    each { |x| comp = false unless yield x }    
+    comp = true
+    each { |x| comp = false unless yield x }
     comp
   end
 
   def any?
-    comp = false;
-    each { |x| comp = true if yield x }    
+    comp = false
+    each { |x| comp = true if yield x }
     comp
   end
 
   def filter
     list = []
-    each { |x| list << x if yield x }    
+    each { |x| list << x if yield x }
     list
   end
 end
--- myList.rb
+++ myList.rb
@@ -1,13 +1,27 @@
+require './MyEnumerable'
+
 class MyList
-  
-  def initialize (*list)
+  include MyEnumerable
+
+  def initialize(*list)
     @list = list
   end
 
-  def each 
+  def each
     i = 0
     while i < @list.length
       yield @list[i]
       i += 1
     end
   end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5 }
+list.all? { |e| e > 5 }
+
+list.any? { |e| e == 2 }
+list.any? { |e| e == 5 }
+
+list.filter { |e| e.even? }
